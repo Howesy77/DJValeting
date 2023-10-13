50ffa2236c3777bead89225c44780793ec0edea1
Paul Howes <paulhowes77@gmail.com>
2023-10-13 09:31:54 +0100

    Initial web project creation

---
+++ src/SixB.CarValeting.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SixB.CarValeting.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
